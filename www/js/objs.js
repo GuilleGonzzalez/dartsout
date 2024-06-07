a1b83c711f261755d694680548b9eac85b2758cc
Guille <guillermo.gonzalez@tychetools.com>
2024-06-07 20:18:05 +0200

    Create Info Card in JS

--- www/js/objs.js
+++ www/js/objs.js
@@ -8,6 +8,8 @@ window.onload = (event) => {
 		let card = createSimpleCard(`Card ${i}`, `Text ${i}`);
 		game.appendChild(card);
 	}
+	let info = createCardInfo();
+	game.appendChild(info);
 }
 
 function createCricketTable(id, rows) {
@@ -61,4 +63,72 @@ function createSimpleCard(headerText, bodyText) {
 	card.appendChild(cardBody);
 
 	return card;
+}
+
+function createCardInfo() {
+	let card = document.createElement("div");
+	card.className = "card mb-4";
+	card.style = "width: 18rem;";
+	let img = document.createElement("img");
+	img.id = "user_img";
+	img.src ="res/user.svg"
+	img.className = "card-img-top";
+	img.height = 100;
+	card.appendChild(img);
+	
+	let infoTitle = createCardInfoTitle("Paco");
+	let infoBody = createCardInfoBody();
+	card.appendChild(infoTitle);
+	card.appendChild(infoBody);
+
+	return card;
+}
+
+function createCardInfoTitle(title) {
+	let cardTitle = document.createElement("h2");
+	cardTitle.className = "card-title text-center fs-2";
+	cardTitle.innerHTML = title;
+
+	return cardTitle;
+}
+
+function createCardInfoBody() {
+	let cardBody = document.createElement("div");
+	cardBody.className = "card-body";
+	let list = document.createElement("ul");
+	list.className = "list-group list-group-flush fs-5 fw-bolder";
+	let listItems = ["Round", "Game score", "Round score"];
+	for (let i = 0; i < listItems.length; i++) {
+		let listItem = document.createElement("li");
+		listItem.className = "list-group-item";
+		listItem.innerHTML = listItems[i];
+		list.appendChild(listItem);
+	}
+	let row = document.createElement("div");
+	row.className = "row";
+	for (let i = 0; i < 3; i++) {
+		let col = document.createElement("div");
+		col.className = "col text-center";
+		let img = document.createElement("img");
+		img.id = `dart${i}_img`;
+		img.src = "res/dart.svg";
+		img.className = "card-img-top";
+		img.height = 50;
+		let br = document.createElement("br");
+		let spanNum = document.createElement("span");
+		spanNum.id = `dart${i}_num`;
+		spanNum.className = "fs-2 fw-bolder";
+		let spanZone = document.createElement("span");
+		spanZone.id = `dart${i}_zone`;
+		col.appendChild(img);
+		col.appendChild(br);
+		col.appendChild(spanNum);
+		col.appendChild(br);
+		col.appendChild(spanZone);
+		row.appendChild(col);
+	}
+	cardBody.appendChild(list);
+	cardBody.appendChild(row);
+
+	return cardBody;
 }
